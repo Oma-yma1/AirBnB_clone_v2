0c5cf0a1d4eb825ae7ce47dcbb64bd4fdc782475
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-04 11:36:29 -0700

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,8 +2,7 @@
 """distribute archive toweb servers"""
 from fabric.api import env, put, run
 import os.path
-env.hosts = ['100.25.33.169', '3.83.18.681']
-
+env.hosts = ['100.25.33.169', '3.83.18.68']
 
 def do_deploy(archive_path):
     """distribute archive to web serverusing do_deploy"""
@@ -16,7 +15,7 @@ def do_deploy(archive_path):
         link = "/data/web_static/current"
         put(archive_path, "/tmp/")
         run("mkdir -p {}".format(nw_vrs))
-        run("tar -xzf /tmp/{} -C {}".format(arcv_fl, nw_vers))
+        run("tar -xzf /tmp/{} -C {}".format(arcv_fl, nw_vrs))
         run("rm /tmp/{}".format(arcv_fl))
         run("mv {}web_static/* {}".format(nw_vrs, nw_vrs))
         run("rm -rf {}web_static".format(nw_vrs))
