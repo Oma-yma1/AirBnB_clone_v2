004a0a5f2f4984a24040320d7ea813c9b116c884
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-04 12:17:18 -0700

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,8 +4,9 @@ from fabric.api import env, put, run
 import os.path
 env.hosts = ['100.25.33.169', '3.83.18.68']
 
+
 def do_deploy(archive_path):
-    """distribute archive to web serverusing do_deploy"""
+    """distribute the  archive to web serverusing do_deploy"""
     if os.path.isfile(archive_path) is False:
         return False
     try:
@@ -15,10 +16,9 @@ def do_deploy(archive_path):
         link = "/data/web_static/current"
         put(archive_path, "/tmp/")
         run("mkdir -p {}".format(nw_vrs))
-        run("tar -xzf /tmp/{} -C {}".format(arcv_fl, nw_vrs))
+        run("tar -xzf /tmp/{} -C {} --strip-components=1".format(arcv_fl, nw_vrs))
         run("rm /tmp/{}".format(arcv_fl))
-        run("mv {}web_static/* {}".format(nw_vrs, nw_vrs))
-        run("rm -rf {}web_static".format(nw_vrs))
+        run("rm -rf {}*".format(nw_vrs))
         run("rm -rf {}".format(link))
         run("ln -s {} {}".format(nw_vrs, link))
         return True
